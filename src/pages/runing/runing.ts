8f1f920b71d8200cb17200980fb57973914d49fa
Rémi GUILLAUME <remi.guillaume.montreal@gmail.com>
2018-04-23 21:48:58 -0400

    Added runing tab

---
+++ src/pages/runing/runing.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-runing',
+  templateUrl: 'runing.html'
+})
+export class RuningPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
